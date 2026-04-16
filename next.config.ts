1a5f64271d6aa9716c176a8bdaca79f4aa62dcdd
Pablo <araujopablo95@gmail.com>
2026-04-16 09:25:45 -0300

    carpeta docs para github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  distDir: "docs",
+  basePath: "/machoaraujotruckandfleetdetailing",
   images: { unoptimized: true },
   allowedDevOrigins: ["192.168.1.3"],
 };
