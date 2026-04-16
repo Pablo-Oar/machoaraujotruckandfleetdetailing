dfa6f0f15adc0ed687e0cd3b4c6cd2455c178f13
Pablo <araujopablo95@gmail.com>
2026-04-16 09:46:00 -0300

    carousel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "docs",
-  basePath: "/machoaraujotruckandfleetdetailing",
+  basePath: isProd ? "/machoaraujotruckandfleetdetailing" : "",
   images: { unoptimized: true },
   allowedDevOrigins: ["192.168.1.3"],
 };
